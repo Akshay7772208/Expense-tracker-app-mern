5dc96b28d47a4eee86d2c731d5c47a3b07623e8e
Akshay7772208 <134836865+Akshay7772208@users.noreply.github.com>
2023-07-10 08:45:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/v1/transactions',require('./routes/transactionRoute'))
 //static files
 app.use(express.static(path.join(__dirname,'./client/build')))
 
-app.get('*',function(req,res)=>{
+app.get('*',function(req,res){
 	res.sendFile(__dirname,'./client/build/index.html')
 })
 //port
